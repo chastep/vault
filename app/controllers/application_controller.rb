da4bef7fd23e445e80bfaf562a0af8450bcdbae9
Chase Stephens <chasestephens92@gmail.com>
2019-03-26 13:11:53 -0500

    forgery test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session
   # skipping CSRF protection is required here to be able to handle requests for js files
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
   include ResponseConcern
   include ExceptionConcern
